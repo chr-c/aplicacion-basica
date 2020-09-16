4b9da91cf3cc33d84131bfbad63bf04a46c1f206
GTPE Student <ccembran-redhat.com@studentvm.6d1a.internal>
2020-09-16 21:00:17 +0000

    otro

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
  echo $_SERVER['SERVER_ADDR'];
  echo "<br><br>nueva liena desde webhook</b>";
  echo "<br><br>prueba 2";
+ echo "<hr>";
  echo "<br><br><b>HOLA GABO</b> ";
+ echo "<hr>";
  echo "</body>";
-
 
 ?>
