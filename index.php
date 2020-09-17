e68c2ab6d8afc2b2b3dd5938a27b06d489c3c9bb
GTPE Student <ccembran-redhat.com@studentvm.6d1a.internal>
2020-09-17 13:38:48 +0000

    cambios

--- index.php
+++ index.php
@@ -7,10 +7,8 @@
  echo "Hola a todos desde <b>";
  echo $_SERVER['SERVER_ADDR'];
  echo "<br><br>nueva liena desde webhook</b>";
+ echo "<hr>";
  echo "<br><br>prueba 2";
- echo "<hr>";
- echo "<br><br><b>HOLA GABO</b> ";
- echo "<hr>";
  echo "</body>";
 
 ?>
