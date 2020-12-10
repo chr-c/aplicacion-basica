67ac66586688f259ca8ebb54ecea9d7dfbd0b57e
chr-c <58703371+chr-c@users.noreply.github.com>
2020-12-10 08:50:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
  echo $_SERVER['SERVER_ADDR'];
  echo "<br><br>nueva liena desde webhook</b>";
  echo "<hr>";
- echo "<br><br>prueba 2";
  echo "<hr>";
- echo "<br><h1>HELLO WORLD</h1>2";
+ echo "<br><h1>HELLO WORLD</h1>";
  echo "</body>";
 
 ?>
