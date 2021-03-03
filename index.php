46019e27aff234f4d1b2928fc7f4883a4021a072
chr-c <58703371+chr-c@users.noreply.github.com>
2021-03-03 17:41:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
  echo $_SERVER['SERVER_ADDR'];
  echo "<br><br>nueva liena desde webhook</b>";
  echo "<hr>";
- echo "<hr>";
- echo "<br><h1>HELLO WORLD</h1>";
- echo "<br><hr><hr><h1>Bienvenido a <b>PUNTO FARMA</b></h1>";
+ echo "<br><h1>Hola a TODOS</h1>";
+ echo "<br><hr><hr><h1>Bienvenido a <b>Banco FIE !!!</b></h1>";
  echo "</body>";
 
 ?>
