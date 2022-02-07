9e78fab16e75ff2c7e0a69eea47cc900bf35e4fb
chr-c <58703371+chr-c@users.noreply.github.com>
2022-02-07 16:02:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  echo "<br>";
  echo "<hr>";
  echo "<br><h1>Hola a TODOS</h1>";
- echo "<br><hr><hr><h1>Bienvenidos a <b> BCB !!!</b></h1>";
+ echo "<br><hr><hr><h1>Bienvenidos a <b> DEMO !!!</b></h1>";
  echo "</body>";
 
 ?>
