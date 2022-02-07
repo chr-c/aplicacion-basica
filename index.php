4f49e7dc128b3c01aa38c282a45d98cac3b46369
chr-c <58703371+chr-c@users.noreply.github.com>
2022-02-07 16:31:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 <?php
  echo "Hola a todos desde <b>";
  echo $_SERVER['SERVER_ADDR'];
- echo "<br>";
+ echo "<hr>";
  echo "<hr>";
  echo "<br><h1>Hola a TODOS</h1>";
- echo "<br><hr><hr><h1>Bienvenidos a <b> DEMO !!!</b></h1>";
+ echo "<br><hr><hr><h1>Bienvenidos a <b> BANCO UNION !!!</b></h1>";
+ echo "<hr>";
  echo "</body>";
 
 ?>
