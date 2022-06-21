d7a7c84195ac9e4852fc52dbd637526de568c55a
chr-c <58703371+chr-c@users.noreply.github.com>
2022-06-21 15:35:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  echo "<hr>";
  echo "<hr>";
  echo "<br><h1>Hola a TODOS version 2</h1>";
- echo "<br><hr><hr><h1>Bienvenidos a <b> Nuestra Institucion !!!</b></h1>";
+ echo "<br><hr><hr><h1>Bienvenidos a <b> Nuestra BNF !!!</b></h1>";
  echo "<hr>";
  echo "</body>";
 
